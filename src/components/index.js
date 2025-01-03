d6eebac8ec5390e3fe7a30a19bec89c9f60f0e41
Manish Kumar Patel <maneeshkurmii@gmail.com>
2025-01-03 12:48:32 +0530

    deleting old files

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Welcome } from './Welcome';
-export { default as Footer } from './Footer';
-
-
-// other components
-export { default as About } from './About';
-export { default as Result } from './Result';
-export { default as Vote } from './Vote';
